77e21307e113b22e56ed43d0d4575b4cd75aa0ea
crazymugg <78958134+crazymugg@users.noreply.github.com>
2023-03-23 11:31:02 +0100

    Documentation Edits
    Removed unnecessary documentation

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ from data.settings import data, char_to_replace
 
 
 
-# Class to handle text and translation functions
+"""
+Base Class for handling Text phrases.
+"""
 class Text():
     def __init__(self, dest, src):
         self.trans = googletrans.Translator()
@@ -42,6 +44,11 @@ class Text():
 
 
 
+"""
+Base Class to work with Excel Documents
+Methods to Load and save documents
+Methods to loop thru entire word documents to update Text Class
+"""
 class Workbook():
     def __init__(self, file):
         self.name = file
@@ -52,48 +59,42 @@ class Workbook():
         self.trans = Text(dest=self.data['destination language'], src=self.data['source language'])
 
 
-    # Method to load the excel document to extract its information
     def load_wb(self):
         wb = openpyxl.load_workbook(f"input/{self.name}")
         self.log_change(content=f'Starting translations for {self.name}')
         return wb
 
 
-    # Method to save word document after changes have been made
     def save_wb(self):
         self.wb.save(f'output/{self.new_name}')
 
 
-    # Method to update log.txt if any changes were made (useful if errors after script was run)
     def log_change(self, content):
         with open('data/log.txt', 'a') as f:
             f.write(f'\n {content}')
 
 
-    # Method to find the last cell (Row and column) that a value occurs
-    # NOTE: This is just the last column and last row that conatin a value. A value may not necessarily be at the row-column combination specified.
-    # e.g. Last item by column is in Z1, last item by row is in B 15, This function would return Z-15, even though there is not a value in that cell.
+    """
+    Method to find the last cell (Row and column) that a value occurs
+    NOTE: This is just the last column and last row that conatin a value. A value may not necessarily be at the row-column combination specified.
+    e.g. Last item by column is in Z1, last item by row is in B 15, This function would return Z-15, even though there is not a value in that cell.
+    """
     def get_lastcell(self):
-            
-            # Finds Last Entry in the last Column
             cols = (tuple(self.ws.columns))
+            row=""
+            col=""
+
             try:
-                # Last entry of the last column of the sheet
-                last = str(cols[-1][-1])
+                last_cell = str(cols[-1][-1])
 
                 # Seperates the last cell's Letter Column and Number Row
-                # NOTE Cell starts with a '.', avoid '.' in the WS title 
+                # NOTE Cell starts with a '.', so avoid '.' in the WS title 
                 try:
-                    split = last.split("'.")
+                    split = last_cell.split("'.")
                 except:
                     print("WorkSheet Name Error! Change worksheet name to avoid the following character combination '.")
                 
-                # setting Last Cell position variable    
                 position = split[1]
-                row=""
-                col=""
-
-                # Seperates the Letter and Number, then assigns them to Col and Row variables respectively
                 for item in position:
                     if item in ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"):
                         col += item
@@ -102,7 +103,7 @@ class Workbook():
                     else:
                         row += item
 
-                #Converts Column string into integer
+                #Converting Column string into integer
                 input = col.lower()
                 output = []
                 for character in input:
@@ -115,7 +116,6 @@ class Workbook():
                 else:
                     col_value = output[0]
 
-                #Return integers for Col and Row
                 return col_value, row
             # If no cells were found, sheet is empty. Return zero coordinates.
             except:
@@ -124,14 +124,8 @@ class Workbook():
 
     # Method to translate worksheet titles (the tabs at the bottom of excel)
     def translate_ws_titles(self):
-
-        # Loop thru each sheet
         for i, sheet in enumerate(self.wb.worksheets):
-            
-            # Register the current title
             old_title = sheet.title
-
-            # Translate title
             new_title = self.trans.translate(sheet.title)
             
             # If translation failed, reset title to old_title
@@ -141,122 +135,81 @@ class Workbook():
             # The character '/' in a WS title crashes excel, so need to remove it    
             if "/" in new_title:
                 resulting = re.sub("/", " ", new_title)
-
-                # Update the WS title
                 self.wb.worksheets[i].title = resulting
-
-                # Update our dictionary, keeping track of the changes made
                 self.ws_titles[old_title] = resulting
                 self.log_change(content=f'Changed WS Title {old_title} to {new_title}')
 
-            # If no special cases, update the WS title
             else:
-                # Update the WS title
                 self.wb.worksheets[i].title = new_title
-
-                # Update our dictionary, keeping track of the changes made
                 self.ws_titles[old_title] = new_title
                 self.log_change(content=f'Changed WS Title {old_title} to {new_title}')
 
 
     def loop_thru_worksheet(self, lcol, lrow):
-        #Loop through the rows of the worksheet
         for row in range(1,(lrow+1)):
-
-            #Nested loop through the columns of the worksheet first
             for col in range(1,(lcol+1)):
-
-                #Get Column text character (e.g column 'AB' = 28)
                 char = get_column_letter(col)
-
-                #Extracts the contents of the cell
                 text = str(self.ws[char + str(row)].value)
 
-
-                # If content starts with '=', check to see if worksheet names are in the equation. If yes, use dict translation. if not then pass
+                # If content starts with '=', check to see if worksheet names are in the excel equation. If yes, use dict translation. if not then pass.
                 if text[0] == "=":
                     translated_check = False
                     self.log_change(content=f'Checking if external sheet reference at {char},{row}')
-
-                    # Loop through the stored Worksheet translations from before
                     for key, value in self.ws_titles.items():
                         if key in text:
-
-                            # Replace found key with already translated WS title
                             translation = text.replace(key, ("'" + value + "'"))
                             self.log_change(content=f'{char}{row} was changed to {translation}')
-
-                            # Reset the cell value with new contents
                             self.ws[char + str(row)].value = translation
                             translated_check = True
-
-                    # If no keys were found in the equation, then pass        
+ 
                     if translated_check == False:
                         self.log_change(content=f'{char}{row}was not translated')
 
-
-                #If text exists, then translate
                 elif text != "None":
                     translation = self.trans.translate(text)
                     self.log_change(content=f'{char}{row} - {translation}')
                     self.ws[char + str(row)].value = translation
 
-                # If cell is empty, then pass
                 else:
                     self.log_change(content=f'{char}{row} - None')
 
 
     def loop_thru_document(self):
-        # Translate all Worksheet titles first
         self.translate_ws_titles()
-
-        # Then translate the content in each Worksheet
         for i, sheet in enumerate(self.wb.worksheets):
             self.log_change(content=f'Starting to translate: {sheet}')
             print(f'Starting to translate: {sheet}')
-
-            # Set the currnet worksheet
             self.ws = self.wb.worksheets[i]
-
-            # find the location of the last cell in the worksheet
             (col_value, row_value) = self.get_lastcell()
-
-            # Translate the entire sheet
             self.loop_thru_worksheet(lcol=int(col_value), lrow=int(row_value)) 
+
         self.log_change(content=f'Completed translating {sheet}')      
         print(f'Completed translating {self.name}')
 
 
 
 
+"""
+General Class for looping thru a folder of Excel documents
+"""
 class App():
     def __init__(self):
         self.input_path = './input'
         self.filename_list = self.get_files()
 
-
-    # Finds and registers all files in the input folder
     def get_files(self):
         filelist = []
-
-        # registers the input directory 
         dirpath = os.listdir(self.input_path)
-
-        # Loop through the input directory
         for file in dirpath:
 
             # Checks if file is valid (aka, not a subfolder)
             if os.path.isfile(os.path.join(self.input_path, file)):
-
-                # Adds the file to the file list
                 filelist.append(file)
 
-        # return the finalized file list
         print(filelist)
         return filelist
 
 
-    # Deletes log file if existing
     def delete_old_log(self):
         if os.path.exists('data/log.txt'):
             os.remove('data/log.txt')
@@ -264,21 +217,12 @@ class App():
             print('Log file not found')
 
 
-    # Main scripting loop through all the files
     def main_loop(self):
         self.delete_old_log()
-        
-        # loop through each file in the input folder
         for file in self.filename_list:
             print(f'\n Loading {file} \n')
-
-            # Initialize document as a word file with the script
             excel = Workbook(file)
-
-            # Loop through the given document
             excel.loop_thru_document()
-
-            # Save the document after the loop is completed in the output folder
             excel.save_wb()
 
 
